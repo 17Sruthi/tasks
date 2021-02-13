cdc39e4a7536f74ba930af0358cc8b6b4f78f375
17Sruthi <78374070+17Sruthi@users.noreply.github.com>
2021-02-14 02:14:30 +0530

    Add files via upload

---
+++ linkedlistBookManagementSystem/mapBookManagementSystem/README.md
@@ -0,0 +1,49 @@
+PROBLEM STATEMENT
+PYTHON PROGRAM USING MAP FOR BOOK MANAGEMENT SYSTEM
+
+TEST CASES
+1. User Menu is displayed and the user is requested to enter their choice as an integer value.
+2. If any other input is given user is notified about it.
+
+LIMITATIONS
+1. None
+
+OUTPUT OF UNITTESTING
+
+Microsoft Windows [Version 10.0.19042.746]
+(c) 2020 Microsoft Corporation. All rights reserved.
+
+C:\Users\HP\Desktop\tasks-main\mapBookManagementSystem>test_functionmapbookmanagementsystem.py
+
+UNIT-TESTING CASE 1: 0 AS INPUT
+.
+UNIT-TESTING CASE 5: BOOLEAN INPUT
+.
+UNIT-TESTING CASE 3: FLOAT INPUT
+.
+UNIT-TESTING CASE 2: NEGATIVE INPUT
+.
+UNIT-TESTING CASE 4: OUT OF RANGE AS INPUT
+.
+UNIT-TESTING CASE 6: STRING INPUT
+.
+UNIT-TESTING CASE 7: VALID INPUT
+.
+UNIT-TESTING CASE 10: 0 AS INPUT
+.
+UNIT-TESTING CASE 8: 0 AS INPUT
+.
+UNIT-TESTING CASE 12: 0 AS INPUT
+.
+UNIT-TESTING CASE 11: 0 AS INPUT
+.
+UNIT-TESTING CASE 9: 0 AS INPUT
+.
+UNIT-TESTING CASE 13: 0 AS INPUT
+.
+----------------------------------------------------------------------
+Ran 13 tests in 0.004s
+
+OK
+
+C:\Users\HP\Desktop\tasks-main\mapBookManagementSystem>
---
+++ linkedlistBookManagementSystem/mapBookManagementSystem/test_functionmapbookmanagementsystem.py
@@ -0,0 +1,79 @@
+import unittest
+
+
+class TestCharAlpha(unittest.TestCase):
+
+
+    def test_choiceinput(self):
+        print("\nUNIT-TESTING CASE 1: 0 AS INPUT")
+        self.assertRaises(ValueError,choiceinput, 0)
+
+    def test_choiceinputnegative(self):
+        print("\nUNIT-TESTING CASE 2: NEGATIVE INPUT")
+        self.assertRaises(ValueError,choiceinput, -6)        
+
+    def test_choiceinputfloat(self):
+        print("\nUNIT-TESTING CASE 3: FLOAT INPUT")
+        self.assertRaises(TypeError,choiceinput,6.23)
+
+    def test_choiceinputoutofrange(self):
+        print("\nUNIT-TESTING CASE 4: OUT OF RANGE AS INPUT")
+        self.assertRaises(ValueError,choiceinput,60)
+
+    def test_choiceinputboolean(self):
+        print("\nUNIT-TESTING CASE 5: BOOLEAN INPUT")
+        self.assertRaises(TypeError,choiceinput, True)
+
+    def test_choiceinputstring(self):
+        print("\nUNIT-TESTING CASE 6: STRING INPUT")
+        self.assertRaises(TypeError,choiceinput, "string")
+
+    def test_choiceinputvalid(self):
+        print("\nUNIT-TESTING CASE 7: VALID INPUT")
+        self.assertIsInstance(choiceinput(3), int)
+
+
+    def test_digitnumberboolean(self):
+        print("\nUNIT-TESTING CASE 8: 0 AS INPUT")
+        self.assertRaises(TypeError,numberofbooks, True)
+
+    def test_digitnumberstring(self):
+        print("\nUNIT-TESTING CASE 9: 0 AS INPUT")
+        self.assertRaises(TypeError,numberofbooks, "string")
+
+    def test_digitnumber(self):
+        print("\nUNIT-TESTING CASE 10: 0 AS INPUT")
+        self.assertRaises(ValueError,numberofbooks, 0)
+
+    def test_digitnumbernegative(self):
+        print("\nUNIT-TESTING CASE 11: 0 AS INPUT")
+        self.assertRaises(ValueError,numberofbooks, -6)
+
+    def test_digitnumberfloat(self):
+        print("\nUNIT-TESTING CASE 12: 0 AS INPUT")
+        self.assertRaises(TypeError,numberofbooks,6.23)
+
+    def test_digitnumbervalid(self):
+        print("\nUNIT-TESTING CASE 13: 0 AS INPUT")
+        self.assertIsInstance(numberofbooks(3), int)
+
+
+def choiceinput(numb):
+    if(type(numb) not in [int]):
+        raise TypeError("NUMBER MUST BE A POSITIVE INTEGER")
+    elif(numb<1 or numb>7):
+        raise ValueError("NUMBER MUST BE IN RANGE [1,7]")
+    return numb 
+
+
+def numberofbooks(number):
+    if(type(number) not in [int]):
+        raise TypeError("INPUT MUST BE A POSITIVE INTEGER")
+    elif(number<1):
+        raise ValueError("NUMBER CANNOT BE NEGATIVE")
+    return number
+
+
+        
+if __name__=='__main__':
+    unittest.main()
